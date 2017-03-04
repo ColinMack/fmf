707428fa0a6223ca36441675266083b30b832ca3
ColinMack <colintmack@gmail.com>
2017-03-03 21:29:55 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ $("#nav button").click( function(event){
     $("#" + $(event.target).val() + "Button").animate({fontSize:'24px'},{ duration: 200, queue: false });
     $("#banner").css("background-color", $(event.target).css("color"));
     $(event.target).css("border-bottom","0px inset "+ $(event.target).css("color"));
+    $(".blockTitle").css("color", $(event.target).css("color"));
     $(event.target).animate({borderBottomWidth:'3px'}, { duration: 200, queue: false });
 });
 
@@ -23,6 +24,7 @@ $(function() {
     $("#tabWrapper .tab").hide();
     $("#homeWrapper").show();
     $("#homeButton").css("font-size", "24px")
-     $("#banner").css("background-color", "rgb(0, 94, 184)");
+    $(".blockTitle").css("color", "rgb(0, 94, 184)");
+    $("#banner").css("background-color", "rgb(0, 94, 184)");
     $("#homeButton").css("border-bottom","3px inset rgb(0, 94, 184)");
 });
