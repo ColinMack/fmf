6d71beabbbbc230c223616a25855ae1c4b05dccf
Colin Mack <colinmack@Colins-MacBook-Pro.local>
2017-03-06 22:04:24 -0800

    starting more things

--- js/piechart.js
+++ js/piechart.js
@@ -8,7 +8,7 @@ var data = [
 		fund_type:"European Stocks",
 		shares: 42,
 		color:"#82bc00"
-	}, 
+	}/*, 
 	{
 		fund_type:"Asian Stocks",
 		shares: 130,
@@ -18,7 +18,7 @@ var data = [
 		fund_type:"Emerging Markets Stocks",
 		shares: 70,
 		color:"orange"
-	}
+	}*/
 ];
 var color = ["#27AFAF", "#82bc00", "#0081C9"]
 
@@ -41,8 +41,14 @@ var loadScatter = function(){
     var default_tooltip = $("#piechart_tooltip_default tbody");
 
     var default_content = default_tooltip.find(".content").empty();
+
+    var total = d3.sum(data.map(function(d) {
+        return d.shares;
+    }));
+
     _.each(data, function(fund){
-    	default_content.append("<tr><td class=\"percent\">" + fund.shares +  "</td><td class=\"label\" style=\"color:" + fund.color + "\">" + fund.fund_type + "</td><tr>");
+        var percent = Math.round(1000 * fund.shares / total) / 10;
+    	default_content.append("<tr><td class=\"percent\">" + percent +  "%</td><td class=\"label\" style=\"color:" + fund.color + "\">" + fund.fund_type + "</td><tr>");
     });
 
     var svg = d3.select("#piechart").append("svg").attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
@@ -55,9 +61,6 @@ var loadScatter = function(){
 
     path.on('mouseover', function(d) {
     	default_tooltip.hide();
-        var total = d3.sum(data.map(function(d) {
-          return d.shares;
-        }));
         var percent = Math.round(1000 * d.data.shares / total) / 10;
         tooltip.find('.label').html(d.data.fund_type);
         tooltip.find('.label').css("color", d.data.color);
