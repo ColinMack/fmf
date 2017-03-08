0fd6fe164d1d9ba4373a75db8744d1a289b55495
ColinMack <colintmack@gmail.com>
2017-03-07 16:24:29 -0800

    Update index.php

--- exp/index.php
+++ exp/index.php
@@ -18,6 +18,13 @@ $connectionInfo = array("UID" => "fmf@fundmyfuture", "pwd" => "blk2017!", "Datab
 $serverName = "tcp:fundmyfuture.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
+  if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+  
   ?>
 
 </body>
