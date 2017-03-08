f1ea4344d1e2ae66b33a0790af4348735cdcd996
ColinMack <colintmack@gmail.com>
2017-03-07 16:17:45 -0800

    Create index.php

---
+++ exp/index.php
@@ -0,0 +1,13 @@
+// PHP Data Objects(PDO) Sample Code:
+try {
+  $conn = new PDO("sqlsrv:server = tcp:fundmyfuture.database.windows.net,1433; Database = FundMyFuture", "fmf", "blk2017!");
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+  print("Error connecting to SQL Server.");
+die(print_r($e));
+}
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "fmf@fundmyfuture", "pwd" => "blk2017!", "Database" => "FundMyFuture", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:fundmyfuture.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
