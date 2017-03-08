2ae9072e4a612a3da86dea239301126738286806
ColinMack <colintmack@gmail.com>
2017-03-07 17:39:49 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,122 @@
 var app = angular.module("fmf", []);
 
+var port_val = {
+    464288596:141.98,
+    464287291:148.94,
+    464285105:40.81
+};
+
+var port_info = {
+    464288596:{
+        market:"United States",
+        asset_class:"Fixed Income",
+        description:"ISHARES GOVERNMENT CREDIT BOND ETF",
+        ticker:"GBF US",
+        currency:"USD",
+        risk_region_country:"United States",
+        SEDOL:"B1MYR16"
+    },
+    464287291:{
+        market:"United States",
+        asset_class:"Equity",
+        description:"ISHARES GLOBAL TECH ETF",
+        ticker:"IXN US",
+        currency:"USD",
+        risk_region_country:"World All",
+        SEDOL:"2819204"
+    },
+    464285105:{
+        market:"United States",
+        asset_class:"Commodity",
+        description:"ISHARES GOLD TRUST",
+        ticker:"IAU US",
+        currency:"USD",
+        risk_region_country:"United States",
+        SEDOL:"B05ND15"
+    }
+};
+
+var user_info = {
+    cmack: {
+        fname:"Colin",
+        lname:"Mack"
+    }
+}
+
+var user_ports = {
+    cmack: [
+        {
+            name:"Colin's College Savings",
+            fund_type:"529",
+            deposited: 1500,
+            contents:[
+                {
+                    cusip:464288596,
+                    shares:5
+                },
+                {
+                    cusip:464287291,
+                    shares:3
+                },
+                {
+                    cusip:464285105,
+                    shares:15
+                }
+            ]
+
+        },
+        {
+            name:"Colin's Investment Portfolio",
+            fund_type:"individual",
+            deposited: 500,
+            contents:[
+                {
+                    cusip:464288596,
+                    shares:5
+                },
+                {
+                    cusip:464287291,
+                    shares:3
+                },
+                {
+                    cusip:464285105,
+                    shares:15
+                }
+            ]
+
+        }/*,
+        {
+            name:"Colin's Test Portfolio",
+            fund_type:"test",
+            deposited: 10000,
+            contents:[
+                {
+                    cusip:464288596,
+                    shares:8
+                },
+                {
+                    cusip:464287291,
+                    shares:4
+                },
+                {
+                    cusip:464285105,
+                    shares:10
+                }
+            ]
+
+        },
+        {
+            name:"Colin's Robo Fund",
+            fund_type:"robo",
+            deposited: 1000
+        }*/
+    ]
+};
+
+var user = "cmack"
+
 $("#nav button").click( function(event){
     window["load" + $(event.target).html() + "Tab"]();
-});
-
-$(".port_manage").click( function(event){
-    loadManageTab($(event.target).closest(".port").find(".port_name").html());
-});
-
-$(".port_compare").click( function(event){
-    loadCompareTab($(event.target).closest(".port").find(".port_name").html());
 });
 
 $(".homeBlockCourse").click(function(event){
@@ -41,18 +148,35 @@ $(function() {
     loadHomeTab();
 });
 
-function loadHomeTab(username){
+function loadHomeTab(){
     $("#tabWrapper .tab").hide();
     $("#nav button").css("border-bottom", "none");
     $("#nav button").css("font-size", "20px");
 
-    $("#banner").html((username != null)?username:"Welcome")
+    $("#banner").html((user_info[user].fname != null)?"Welcome, " + user_info[user].fname + "!":"Welcome")
 
     $("#homeWrapper").show();
     $("#homeButton").animate({fontSize:'24px'},{ duration: 200, queue: false });
     $("#banner").css("background-color", "rgb(0, 94, 184)");
     $("#homeButton").css("border-bottom","0px inset rgb(0, 94, 184)");
     $("#homeButton").animate({borderBottomWidth:'3px'}, { duration: 200, queue: false });
+
+_.each(user_ports["cmack"], function(port, index) {
+        var tmpl = _.template($('#port-template').html());
+        var elem = $(tmpl(port));
+        var row = $("#port_row" + Math.floor(index/2+1) + " #port" + (index%2+1));
+        row.ready(function() {
+            row.html(elem.get(0));
+            $(".port_manage").click( function(event){
+                loadManageTab($(event.target).closest(".port").find(".port_name").html());
+            });
+
+            $(".port_compare").click( function(event){
+                loadCompareTab($(event.target).closest(".port").find(".port_name").html());
+            });
+        });
+    });
+    
 }
 
 function loadManageTab(portname){
@@ -111,5 +235,5 @@ function loadCompareTab(portname){
     $("#compareButton").css("border-bottom","0px inset rgb(39, 175, 175)");
     $("#compareButton").animate({borderBottomWidth:'3px'}, { duration: 200, queue: false });
 
-    loadScatter();
+    loadPieChart(_.filter(user_ports[user], function(d){if(d.name==portname)return d;}));
 }
