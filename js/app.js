4a80a91751434d46b08efa99e8446ee2446024b3
Colin Mack <colinmack@Colins-MacBook-Pro.local>
2017-03-07 22:03:17 -0800

    lots of improvements to compare tab

--- js/app.js
+++ js/app.js
@@ -6,7 +6,16 @@ var port_val = {
     464285105:40.81
 };
 
+var port_val_year_start = {
+    464288596:135.50,
+    464287291:101.36,
+    464285105:35.90
+};
+
 var port_info = {
+    0:{
+        description:"FMF ROBO-ADVISED FUND",
+    },
     464288596:{
         market:"United States",
         asset_class:"Fixed Income",
@@ -40,6 +49,10 @@ var user_info = {
     cmack: {
         fname:"Colin",
         lname:"Mack"
+    },
+    wbuck: {
+        fname:"Weston",
+        lname:"Buck"
     }
 }
 
@@ -72,11 +85,11 @@ var user_ports = {
             contents:[
                 {
                     cusip:464288596,
-                    shares:5
+                    shares:25
                 },
                 {
                     cusip:464287291,
-                    shares:3
+                    shares:13
                 },
                 {
                     cusip:464285105,
@@ -84,7 +97,7 @@ var user_ports = {
                 }
             ]
 
-        }/*,
+        },
         {
             name:"Colin's Test Portfolio",
             fund_type:"test",
@@ -109,7 +122,74 @@ var user_ports = {
             name:"Colin's Robo Fund",
             fund_type:"robo",
             deposited: 1000
-        }*/
+        }
+    ],
+    wbuck: [
+        {
+            name:"Weston's College Savings",
+            fund_type:"529",
+            deposited: 1500,
+            contents:[
+                {
+                    cusip:464288596,
+                    shares:5
+                },
+                {
+                    cusip:464287291,
+                    shares:3
+                },
+                {
+                    cusip:464285105,
+                    shares:15
+                }
+            ]
+
+        },
+        {
+            name:"Weston's Investment Portfolio",
+            fund_type:"individual",
+            deposited: 500,
+            contents:[
+                {
+                    cusip:464288596,
+                    shares:25
+                },
+                {
+                    cusip:464287291,
+                    shares:13
+                },
+                {
+                    cusip:464285105,
+                    shares:15
+                }
+            ]
+
+        },
+        {
+            name:"Weston's Test Portfolio",
+            fund_type:"test",
+            deposited: 10000,
+            contents:[
+                {
+                    cusip:464288596,
+                    shares:8
+                },
+                {
+                    cusip:464287291,
+                    shares:4
+                },
+                {
+                    cusip:464285105,
+                    shares:10
+                }
+            ]
+
+        },
+        {
+            name:"Weston's Robo Fund",
+            fund_type:"robo",
+            deposited: 1000
+        }
     ]
 };
 
@@ -140,8 +220,7 @@ $("#goToSocialStatsButton").click(function(event){
 });
 
 $(".compareNavPort").click(function(event){
-    $(".compareNavPort.selected").removeClass("selected");
-    $(event.target).closest(".compareNavPort").addClass("selected");
+
 });
 
 $(function() {
@@ -161,9 +240,20 @@ function loadHomeTab(){
     $("#homeButton").css("border-bottom","0px inset rgb(0, 94, 184)");
     $("#homeButton").animate({borderBottomWidth:'3px'}, { duration: 200, queue: false });
 
-_.each(user_ports["cmack"], function(port, index) {
+    _.each(user_ports["cmack"], function(port, index) {
         var tmpl = _.template($('#port-template').html());
         var elem = $(tmpl(port));
+        var sum = 0;
+        var bp_sum = 0;
+        _.each(port.contents, function(port){
+            bp_sum += port_val_year_start[port.cusip] * port.shares;
+            sum += port_val[port.cusip] * port.shares;
+        });
+        elem.find(".port_val").html("$" + Math.round(sum*100)/100);
+        
+        elem.find(".port_bps").html((port.fund_type != "robo")?(Math.round(((sum-bp_sum)/bp_sum)*100)/100 + "%"):"");
+
+
         var row = $("#port_row" + Math.floor(index/2+1) + " #port" + (index%2+1));
         row.ready(function() {
             row.html(elem.get(0));
@@ -184,7 +274,7 @@ function loadManageTab(portname){
     $("#nav button").css("border-bottom", "none");
     $("#nav button").css("font-size", "20px");
 
-    $("#banner").html((portname != null)?portname:"Portfolio 1")
+    $("#banner").html((portname != null)?portname:user_ports[user][0].name)
 
     $("#manageWrapper").show();
     $("#manageButton").animate({fontSize:'24px'},{ duration: 200, queue: false });
@@ -227,7 +317,26 @@ function loadCompareTab(portname){
     $("#nav button").css("border-bottom", "none");
     $("#nav button").css("font-size", "20px");
 
-    $("#banner").html((portname != null)?portname:"Portfolio 1")
+    $("#banner").html((portname != null)?portname:user_ports[user][0].name)
+
+    loadPieChart((portname != null)?(_.filter(user_ports[user], function(d){if(d.name==portname)return d;}))[0]:user_ports[user][0]);
+
+    $("#compareNav").empty();
+    _.each(user_ports["cmack"], function(port, index) {
+        var tmpl = _.template($('#compareNav-template').html());
+        var elem = $(tmpl(port));
+        if(index==0){
+            elem.addClass("selected");
+        }
+        $("#compareNav").append(elem);
+    });
+    $(".compareNavPort").click(function(event) {
+        $(".compareNavPort.selected").removeClass("selected");
+        var navPort = $(event.target).closest(".compareNavPort")
+        navPort.addClass("selected");
+        loadPieChart(_.filter(user_ports[user], function(d){if(d.name==navPort.find(".portName").html())return d;})[0]);
+        $("#banner").html(navPort.find(".portName").html());
+    });
 
     $("#compareWrapper").show();
     $("#compareButton").animate({fontSize:'24px'},{ duration: 200, queue: false });
@@ -235,5 +344,5 @@ function loadCompareTab(portname){
     $("#compareButton").css("border-bottom","0px inset rgb(39, 175, 175)");
     $("#compareButton").animate({borderBottomWidth:'3px'}, { duration: 200, queue: false });
 
-    loadPieChart(_.filter(user_ports[user], function(d){if(d.name==portname)return d;}));
+    
 }
--- js/piechart.js
+++ js/piechart.js
@@ -1,36 +1,30 @@
-/*var data = [
-	{
-		fund_type:"U.S. Stocks",
-		shares: 35,
-		color: "#27AFAF"
-	}, 
-	{
-		fund_type:"European Stocks",
-		shares: 42,
-		color:"#82bc00"
-	}/*, 
-	{
-		fund_type:"Asian Stocks",
-		shares: 130,
-		color:"#0081C9"
-	},
-	{
-		fund_type:"Emerging Markets Stocks",
-		shares: 70,
-		color:"orange"
-	}
-];*/
-var color = ["#27AFAF", "#82bc00", "#0081C9", "#006747", "#0093639", "#B4DB0A", "#FFCC00", "#FCD015", "#CB333B", "#671E75"]
+var colors = ["#27AFAF", "#82bc00", "#0081C9", "#006747", "#0093639", "#B4DB0A", "#FFCC00", "#FCD015", "#CB333B", "#671E75"]
 
 var loadPieChart = function(data, category){
-	var ports = data[0].contents;
+    if(data.fund_type == "robo"){
+        var ports = [{
+            cusip:0,
+            shares:100
+        }]
+    }
+    else{
+        var ports = data.contents;
+    }
 
+    var filterData = {};
 	_.each(ports, function (port){
-		var pieData = jQuery.extend({}, port_info[port.cusip]);
-		pieData["shares"] = port.shares;
-		return;
-
+        var port_category = port_info[port.cusip][(category)?category:"description"];
+		filterData[port_category] = (filterData[port_category])?  (filterData[port_category] + port.shares):port.shares;
 	});
+
+    var pieData = [];
+    _.each(Object.keys(filterData), function(group, index){
+        pieData.push({
+            name:group,
+            shares:filterData[group],
+            color:colors[index]
+        });
+    });
 
     $("#piechart").empty();
 
@@ -51,18 +45,18 @@ var loadPieChart = function(data, category){
 
     var default_content = default_tooltip.find(".content").empty();
 
-    var total = d3.sum(data.map(function(d) {
+    var total = d3.sum(pieData.map(function(d) {
         return d.shares;
     }));
 
-    _.each(data, function(fund){
+    _.each(pieData, function(fund){
         var percent = Math.round(1000 * fund.shares / total) / 10;
-    	default_content.append("<tr><td class=\"percent\">" + percent +  "%</td><td class=\"label\" style=\"color:" + fund.color + "\">" + fund.fund_type + "</td><tr>");
+    	default_content.append("<tr><td class=\"percent\">" + percent +  "%</td><td class=\"label\" style=\"color:" + fund.color + "\">" + fund.name + "</td><tr>");
     });
 
     var svg = d3.select("#piechart").append("svg").attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    var g = svg.selectAll(".arc").data(pie(data)).enter().append("g").attr("class", "arc");
+    var g = svg.selectAll(".arc").data(pie(pieData)).enter().append("g").attr("class", "arc");
 
     var path = g.append("path").attr("d", arc).style("fill", function(d) { 
         return d.data.color; 
@@ -71,7 +65,7 @@ var loadPieChart = function(data, category){
     path.on('mouseover', function(d) {
     	default_tooltip.hide();
         var percent = Math.round(1000 * d.data.shares / total) / 10;
-        tooltip.find('.label').html(d.data.fund_type);
+        tooltip.find('.label').html(d.data.name);
         tooltip.find('.label').css("color", d.data.color);
         tooltip.find('.shares').html(d.data.shares + " Shares");
         tooltip.find('.percent').html(percent + "%");
